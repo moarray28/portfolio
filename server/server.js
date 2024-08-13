3b73b076fd9945ef733698773ba5d8211b2522d9
moarray28 <moreritesh3969@gmail.com>
2024-08-13 20:40:32 +0530

    added vercel.json

--- server/server.js
+++ server/server.js
@@ -1,20 +1,17 @@
- // server.js
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
-const app = express();
-const port = 3000;
-
 const cors = require('cors');
 
-app.use(cors(
-    {
-      origin:process.env.VITE_FRONTEND_URL,
-        credentials:true
-    }
-));// Middleware to parse JSON bodies
+const app = express();
+const port = 3000; // Use the port from environment variables if available
+
+app.use(cors({
+  origin: process.env.VITE_FRONTEND_URL || '*', // Fallback to '*' if not defined
+  credentials: true
+}));
+
 app.use(bodyParser.json());
 
 // Endpoint to handle form submissions
@@ -25,15 +22,15 @@ app.post('/api/contact', async (req, res) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail', // or any other email service
     auth: {
-      user: `${process.env.VITE_EMAIL}`,
-      pass: `${process.env.VITE_EMAIL_PASSWORD}` //google step 2 app password
+      user: process.env.VITE_EMAIL,
+      pass: process.env.VITE_EMAIL_PASSWORD // Google App password
     }
   });
 
   // Set up email data
   const mailOptions = {
     from: email,
-    to:`${process.env.VITE_EMAIL}`,
+    to: process.env.VITE_EMAIL,
     subject: `Contact form submission from ${name}`,
     text: message,
     html: `<p>From: ${name} (${email})</p><p>${message}</p>`
@@ -52,5 +49,3 @@ app.post('/api/contact', async (req, res) => {
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
-
-
